bfeef6ab0dae195af5ccabb2353ff220f6bf1b6b
vijay <evijay77@gmail.com>
2019-02-04 23:24:21 -0800

    secnd

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
+// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-   res.send('Hello World');
+  res.send('hello world')
 })
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(3000);
